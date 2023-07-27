6e6f018477ba0a5732808d1c9f8e73b361ff4322
Yassen <nwbyaseen@gmail.com>
2023-07-27 14:43:45 +0500

    header convert into react

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import "./style.css";
+import"./utility.css";
 
-function App() {
-  return <h1>Hello world!</h1>;
+function App(){
+  return (
+   <header className="header">
+  <div className="head">
+          <img src="logo.png" alt="today i learned" height="60vh" width="60vw" />
+          <h1>today i learned</h1>
+  </div>
+<button className="btn btn-large btn-open ">Share a Fact</button>
+</header>
+  );
 }
-
 export default App;
