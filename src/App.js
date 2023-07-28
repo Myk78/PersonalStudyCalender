7f910c069a5aa7d2fc390fb32631d5aa6bf448a6
Yassen <nwbyaseen@gmail.com>
2023-07-28 23:40:02 +0500

    hello

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import"./utility.css";
 
 function App(){
   return (
+    <>
    <header className="header">
   <div className="head">
           <img src="logo.png" alt="today i learned" height="60vh" width="60vw" />
@@ -10,6 +11,15 @@ function App(){
   </div>
 <button className="btn btn-large btn-open ">Share a Fact</button>
 </header>
+<ChoiceFrom/>
+</>
+  );
+}
+function ChoiceFrom(){
+  return(
+  <form class="choicefrom hidden ">
+       fact form
+    </form>
   );
 }
 export default App;
